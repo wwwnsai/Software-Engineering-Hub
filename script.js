eef0f975d610707d4901ad1db0143c64dbe9e37e
Thatchapon Chatthanes <chatthanes22@gmail.com>
2023-10-14 18:14:24 +0700

    Yed Doo 3

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-let header = document.querySelector(".navigation");
+// Navigation
+const header = document.querySelector(".navigation");
+
+// About
+const aboutSelections = document.querySelectorAll(
+    "[id^='about__selection-element--']"
+);
+const aboutDetails = document.querySelectorAll(
+    "[id^='about__details-element--']"
+);
+
+// Lecturers
 const boxes = document.querySelectorAll(".lecturers__box");
 
+// Function
+
 // Navigation Bar Scroll Fixed
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", () => {
     header.classList.toggle("sticky", window.scrollY);
 });
 
+// About
+aboutSelections.forEach((selection, index) => {
+    selection.addEventListener("click", () => {
+        aboutDetails.forEach((detail, detailIndex) => {
+            detail.style.display = index === detailIndex ? "block" : "none";
+        });
+    });
+});
+
 // Lecturers Hover
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
     let activeBox = null;
 
-    boxes.forEach(function (box) {
-        box.addEventListener("mouseenter", function () {
+    boxes.forEach((box) => {
+        box.addEventListener("mouseenter", () => {
             if (activeBox) {
                 activeBox.classList.remove("lecturers__box--active");
             }
