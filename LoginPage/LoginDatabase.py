502bf0f3a5986e6dfadf225c2c821cdd351fb7d7
wwwnsai <65011588@kmitl.ac.th>
2023-10-17 15:14:20 +0700

    login & register page

--- LoginPage/LoginDatabase.py
+++ LoginPage/LoginDatabase.py
@@ -46,7 +46,7 @@ def login(username: str = Form(...), password: str = Form(...)):
             if user_data.password == password:
                 user_data.is_logged_in = True  # Set to True when the user logs in
                 transaction.commit()
-                return {"message": "Login succesful"}
+                return {"message": "Login successful"}
             else:
                 return {"message": "Incorrect password"}
         else:
--- LoginPage/main.js
+++ LoginPage/main.js
@@ -1,13 +1,13 @@
-const login = function () {
-    const username = document.getElementById("username-login").value;
-    const password = document.getElementById("password-login").value;
+function login() {
+    const username = document.getElementById('username-login').value;
+    const password = document.getElementById('password-login').value;
 
     const formData = new FormData();
-    formData.append("username", username);
-    formData.append("password", password);
+    formData.append('username', username);
+    formData.append('password', password);
 
     const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "http://127.0.0.1:8000/login");
+    xhttp.open('POST', 'http://127.0.0.1:8000/login');
     xhttp.send(formData);
 
     xhttp.onreadystatechange = function () {
@@ -15,35 +15,36 @@ const login = function () {
             if (this.status == 200) {
                 const response = JSON.parse(this.responseText);
                 console.log(response);
-                if (response.message == "Login succesful") {
-                    // Store the username in localStorage if login is successful.
-                    localStorage.setItem("user", username);
+                if (response.message == 'Login successful') {
+                    localStorage.setItem('user', username);
+                    window.location.href = '../Main/main.html';
                 } else {
                     alert(response.message);
                 }
             } else {
-                alert("Login failed. Check your credentials.");
+                alert('Login failed. Check your credentials.');
             }
         }
-    };
-};
+    }
+    
+}
 
-const logout = function () {
-    localStorage.removeItem("user");
-};
+function logout() {
+    localStorage.removeItem('user');
+}
 
-const register = function () {
-    const username = document.getElementById("username-register").value;
-    const email = document.getElementById("email-register").value;
-    const password = document.getElementById("password-register").value;
+function register() {
+    const username = document.getElementById('username-register').value;
+    const email = document.getElementById('email-register').value;
+    const password = document.getElementById('password-register').value;
 
     const formData = new FormData();
-    formData.append("username", username);
-    formData.append("email", email);
-    formData.append("password", password);
+    formData.append('username', username);
+    formData.append('email', email);
+    formData.append('password', password);
 
     const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "http://127.0.0.1:8000/register");
+    xhttp.open('POST', 'http://127.0.0.1:8000/register');
     xhttp.send(formData);
 
     xhttp.onreadystatechange = function () {
@@ -51,23 +52,23 @@ const register = function () {
             if (this.status == 200) {
                 const response = JSON.parse(this.responseText);
                 console.log(response);
-                if (response.message == "Registration successful") {
-                    window.location.href = "index.html";
+                if (response.message == 'Registration successful') {
+                    window.location.href = 'login.html';
                 } else {
                     alert(response.message);
                 }
             } else {
-                alert("Registration failed. Try again.");
+                alert('Registration failed. Try again.');
             }
         }
-    };
-};
+    }
+}
 
-const getInfo = function () {
-    const username = localStorage.getItem("user");
+function getInfo(){
+    const username = localStorage.getItem('user');
 
     const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", `http://127.0.0.1:8000/user/${username}`);
+    xhttp.open('GET', `http://127.0.0.1:8000/user/${username}`);
     xhttp.send();
 
     xhttp.onreadystatechange = function () {
@@ -76,16 +77,15 @@ const getInfo = function () {
                 const response = JSON.parse(this.responseText);
                 console.log(response);
                 if (response.username == username) {
-                    document.getElementById("username-info").innerHTML =
-                        response.username;
-                    document.getElementById("email-info").innerHTML =
-                        response.email;
+                    document.getElementById('username-info').innerHTML = response.username;
+                    document.getElementById('email-info').innerHTML = response.email;
                 } else {
                     alert(response.message);
                 }
             } else {
-                alert("User not found.");
+                alert('User not found.');
             }
         }
-    };
-};
+    }
+}
+
