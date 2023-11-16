a032c3b6950d9ba32b97da51e12525578ad535ff
wwwnsai <65011588@kmitl.ac.th>
2023-11-16 21:43:49 +0700

    User Info 1.0

--- main/templates/UserInfo/main.js
+++ main/templates/UserInfo/main.js
@@ -1,15 +1,22 @@
 const idField = document.getElementById("userinfo__id");
 const usernameField = document.getElementById("userinfo__username");
 const emailField = document.getElementById("userinfo__email");
+const itemsField = document.getElementById("userinfo__items");
 
 function fetchData() {
     fetch(`/userinfo`, {})
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
-            idField.innerHTML = `Student ID: ${data.user.id}`;
-            usernameField.innerHTML = `Username: ${data.user.username}`;
-            emailField.innerHTML = `Email: ${data.user.email}`;
+            let items = data.user.items;
+            console.log(items);
+            if (items.length === 0) {
+                items = "-";
+            }
+            idField.innerHTML = `Student ID: &emsp; ${data.user.id}`;
+            usernameField.innerHTML = `Username: &emsp; ${data.user.username}`;
+            emailField.innerHTML = `Email: &emsp; &emsp; &emsp; ${data.user.email}`;
+            itemsField.innerHTML = `Items: &emsp; &emsp; &emsp; ${items}`;
         })
         .catch((error) => {
             console.error("Error fetching user information:", error);
