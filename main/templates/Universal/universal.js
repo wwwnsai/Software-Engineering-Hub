f6dfb07da5305e2a6d03b7dd05b21c5ef8cab98a
Thatchapon Chatthanes <71886088+PomPerWoW@users.noreply.github.com>
2023-11-16 23:57:57 +0700

    Yed Doo Master 19

--- main/templates/Universal/universal.js
+++ main/templates/Universal/universal.js
@@ -1,5 +1,14 @@
 import { updatePageContent } from "./js/user.js";
 
+document.addEventListener("DOMContentLoaded", function () {
+    var hamburgerMenu = document.querySelector(".hamburger-menu");
+    var mobileMenu = document.querySelector(".mobile-menu");
+
+    hamburgerMenu.addEventListener("click", function () {
+        mobileMenu.style.display =
+            mobileMenu.style.display === "block" ? "none" : "block";
+    });
+});
 // Initial function
 function init() {
     updatePageContent();
